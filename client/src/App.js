13e95eb38527e07289f2e111f69e39b085cf5445
unknown <mauro.hdz99@gmail.com>
2019-10-12 12:21:00 -0500

    Created front-end setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route, Link} from "react-router-dom"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+    <div className="container">
+      <h1>MERN APP IN PROGRESS</h1>
     </div>
+    <Route path="/" exact component={List} />
+    <Route path="/edit/:id" component={EditList} />
+    <Route path="/create" component={Create} />
+
+    </Router>
   );
 }
 
