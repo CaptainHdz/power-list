91fc095886587254420f11806f2f354732174719
unknown <mauro.hdz99@gmail.com>
2019-10-13 00:20:27 -0500

    Server is started, MongoDB is connected

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const PORT = process.env.PORT || 4000;
+
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/todos";
+mongoose.connect(MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true})
+.then(() => console.log("MONGODB is now connected"))
+.catch(err => console.log("MONGODB ERROR: " + err))
+
+
+app.listen(PORT, () => {
+    console.log(`Server listening on PORT: ${PORT}`)
+})
