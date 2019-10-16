52b8b15b09662d3f22968a4c9346b8cd0e332f94
unknown <mauro.hdz99@gmail.com>
2019-10-15 22:36:03 -0500

    Added api route

--- controller/todoController.js
+++ controller/todoController.js
@@ -5,6 +5,11 @@ const TodoItem = require("../models/todo");
 module.exports = {
     findAll: (req, res) => {
         //Some fnacy route code
+        TodoItem.find((err, todos) => {
+            if (err) {console.log("DB error: " + err)}
+            else {console.log(todos)};
+            res.json(todos)
+        })
     },
     Create: (req, res) => {
         //Some more fancy code 
--- routes/todo.js
+++ routes/todo.js
@@ -1,11 +1,14 @@
-const app = require("express");
-const router = app.Router()
+const express = require("express");
+
+const router = express.Router()
 const mongoose = require("mongoose");
 const todoController = require("../controller/todoController");
 
 router.route("/")
 .get(todoController.findAll)
 .post(todoController.Create);
+
+router.route("/Create")
 
 module.exports = router;
 
