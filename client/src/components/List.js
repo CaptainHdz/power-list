6108c3f0a607f5be84a1347ac037c0831638313b
unknown <mauro.hdz99@gmail.com>
2019-10-28 22:45:08 -0500

    Working db retrieval

--- client/src/components/List.js
+++ client/src/components/List.js
@@ -1,6 +1,20 @@
 import React, {Component} from "react";
+import {Link} from 'react-router-dom';
+import axios from 'axios';
 
 class List extends Component {
+    
+constructor (props) {
+    super(props);
+    this.state = {
+        todos: []
+    };
+}
+
+componentDidMount() {
+    axios.get("/todos/")
+    .then((response)=> console.log(response))
+}
     render() {
         return (
             <div>
