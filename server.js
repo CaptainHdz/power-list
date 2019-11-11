7103f5add94a25ecba393f74133b5955a50baa40
unknown <mauro.hdz99@gmail.com>
2019-11-11 00:13:22 -0600

    Added more prod code

--- server.js
+++ server.js
@@ -34,6 +34,10 @@ if (process.env.NODE_ENV === 'production') {
       res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
   }
+//More production code for routes
+  router.use(function(req, res) {
+    res.sendFile( path.join(__dirname, '../client/build/index.html'));
+  });
 
 
 app.listen(PORT, () => {
