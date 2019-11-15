1759399f5c7b7dadc759d16148d6332f734e85e2
unknown <mauro.hdz99@gmail.com>
2019-11-15 15:39:58 -0600

    Minor ui bug

--- client/src/components/QuoteCard.js
+++ client/src/components/QuoteCard.js
@@ -14,13 +14,13 @@ class Card extends Component {
 
     componentDidMount() {
         const quotes = [
-            {quote: 'You miss 100% of the shots you dont take', author: 'Wayne Gretzky'},
-            {quote: 'If you cannot do great things, do small things in a great way', author: 'Napoleon Hill'},
-            {quote: 'If not us, who? If not now, when?', author: 'Hillel the Elder'},
-            {quote: 'He that is good for making excuses, is seldom good for anything else', author: 'Benjamin Franklin'},
-            {quote: 'We are what we repeatedly do. Excellence, then, is not an act, but a habit', author: 'Aristotle'},
-            {quote: 'If you spend too much time thinking about a thing, you will never get it done', author: 'Bruce Lee'},
-            {quote: 'You cannot build a reputation on what you are going to do', author: 'Henry Ford'},
+            {quote: 'You miss 100% of the shots you dont take.', author: 'Wayne Gretzky'},
+            {quote: 'If you cannot do great things, do small things in a great way.', author: 'Napoleon Hill'},
+            {quote: 'If not us, who? If not now, when?.', author: 'Hillel the Elder'},
+            {quote: 'He that is good for making excuses, is seldom good for anything else.', author: 'Benjamin Franklin'},
+            {quote: 'We are what we repeatedly do. Excellence, then, is not an act, but a habit.', author: 'Aristotle'},
+            {quote: 'If you spend too much time thinking about a thing, you will never get it done.', author: 'Bruce Lee'},
+            {quote: 'You cannot build a reputation on what you are going to do.', author: 'Henry Ford'},
             {quote: 'Make a list. Do the list. Win today.', author: 'Andy Frisella'}
     ];
 
@@ -35,7 +35,7 @@ class Card extends Component {
 
     render() {
         return (
-            <div className="card mt-5 quote-card mx-auto border-dark">
+            <div className="card mt-5 quote-card mx-auto border-0 m-5 fixed-bottom">
   <div className="card-header bg-dark text-light">
     Get Inspired
   </div>
